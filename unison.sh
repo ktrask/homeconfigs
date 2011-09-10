5bf4dafb5ad3ba5282eb56b59fa9e36c0f648c16
Heinrich Schmidt <heinrich.schmidt@tu-bs.de>
2011-09-10 04:28:37 +0200

    add a unison file

---
+++ unison.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+unison /home/ktrask/progs/Git/homeconfigs /home/ktrask \
+    -path .vim/	    \
+    -path .config/awesome/  \
+    -path .oh-my-zsh \
+    -path .unison.sh.swp \
+    -path .vimrc \
+    -path .Xdefaults \
+    -path .zsh \
+    -path .zshenv \
+    -path .zshrc \
+    -path .zsh-update \
